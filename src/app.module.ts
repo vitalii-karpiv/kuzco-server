96b672995ec4701c04a9f69b7720709e4bf4945e
Vitalii Karpiv <vitalia.karpiv@gmail.com>
2024-04-14 14:54:56 +0300

    Supplier entity first steps

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from "@nestjs/common";
 import { OrderModule } from "./order/order.module";
+import { SupplierModule } from "./supplier/supplier.module";
 
 @Module({
-  imports: [OrderModule],
+  imports: [OrderModule, SupplierModule],
   controllers: [],
   providers: [],
 })
