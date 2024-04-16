1e94ad6584047cada9c690e6ce658c88ff2777a8
Vitalii Karpiv <vitalia.karpiv@gmail.com>
2024-04-16 18:02:20 +0300

    Clean up, fix minor bug

--- src/middleware/state-checker.middleware.ts
+++ src/middleware/state-checker.middleware.ts
@@ -9,7 +9,6 @@ export class StateCheckerMiddleware implements NestMiddleware {
   constructor(private readonly kuzcoService: KuzcoService) {}
 
   async use(req: Request, res: Response, next: NextFunction) {
-    console.log("Request...");
     const kuzco = await this.kuzcoService.get();
     if (kuzco.state !== KuzcoState.active && !this.#isReadAllowed(kuzco, req)) {
       throw new BadRequestException({
