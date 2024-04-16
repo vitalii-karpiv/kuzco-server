028f97ebcae9cb86e15c2e69690e92df1d6c2227
Vitalii Karpiv <vitalia.karpiv@gmail.com>
2024-04-16 11:59:11 +0300

    Add configurations to the project

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,14 +5,25 @@ import { APP_PIPE } from "@nestjs/core";
 import { ValidationPipe } from "./common/utils/validation.pipe";
 import { MongooseModule } from "@nestjs/mongoose";
 import { KuzcoModule } from "./kuzco/kuzco.module";
+import * as process from "process";
+import { ConfigModule, ConfigService } from "@nestjs/config";
 
 @Module({
-  //
   imports: [
     OrderModule,
     SupplierModule,
     KuzcoModule,
-    MongooseModule.forRoot(`mongodb+srv://vitaliakarpiv:RSkauTqBtGMWvrf6@local.zhtudpk.mongodb.net/local_dev`),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>("MONGODB_URI"),
+      }),
+      inject: [ConfigService],
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `env/.env.${process.env.NODE_ENV}`,
+    }),
   ],
   controllers: [],
   providers: [
